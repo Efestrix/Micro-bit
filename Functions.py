0c58494e6df1a4a55c047fe14891bfa9c69098ec
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2023-06-15 15:21:31 +0200

    Functions.py

---
+++ Functions.py
@@ -0,0 +1,38 @@
+def print_twice():
+    print("hello")
+    print("world")
+
+print_twice()
+
+def sum(number1, number2):
+    result = number1 + number2
+    return result
+
+print(sum(3,5))
+
+
+
+def one():
+    return 1
+
+print(one())
+
+
+
+def sum (number1, number2):
+    result = number1 * number2
+    return result
+
+print(sum(8,5))
+
+def square(number1, number2):
+    result = number1 + number2
+    return result
+
+print(square(3,5))
+
+def multiplication(number1):
+    result = number1 * number1
+    return result
+
+print(multiplication(8))
