188d613d4df89801a9a449fa784f5dc9bd8da9dd
Jakub Strakos <94374098+Efestrix@users.noreply.github.com>
2023-06-15 15:23:32 +0200

    Hello.py

---
+++ Hello.py
@@ -0,0 +1,18 @@
+from microbit import *
+
+display.scroll("Hello world!")
+sleep(1000)
+
+display.show("A")
+display.show("B")
+display.show("C")
+
+
+display.show(Image.HAPPY)
+boat = Image("05050:"
+             "05050:"
+             "05050:"
+             "99999:"
+             "09990")
+
+display.show(boat)
